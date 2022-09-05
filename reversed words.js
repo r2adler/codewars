cd4966c4de71e161391f19d99c705e6324375b8f
Artur Khareksian <105168210+ArtuAdler@users.noreply.github.com>
2022-09-05 12:17:55 +0300

    Delete reversed words.js

--- reversed words.js
+++
@@ -1,9 +0,0 @@
-function reverseWords(str){
-  const array = str.split(' ');
-  console.log(array)
-  let result = [];
-  for (let i = array.length - 1; i >= 0; i -= 1) {
-    result.push(array[i]);
-  }
-  return result.join(' ');
-}
